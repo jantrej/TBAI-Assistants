d6329ff5db8a805a5bb527e63749ee2640e4aacf
jantrej <jantrejbal7@gmail.com>
2024-12-14 22:48:56 +0100

    Create page-creative.tsx

---
+++ src/app/page-creative.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Creative from '@/components/ui/Creative';
+
+export default function CreativePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Creative />
+    </main>
+  );
+}
