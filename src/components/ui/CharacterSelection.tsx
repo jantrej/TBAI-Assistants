7130b556a18bbd58b3093545eb28d2fb2e8e8542
jantrej <jantrejbal7@gmail.com>
2024-12-15 23:12:21 +0100

    Update CharacterSelection.tsx

--- src/components/ui/CharacterSelection.tsx
+++ src/components/ui/CharacterSelection.tsx
@@ -562,7 +562,7 @@ function LockedOverlay({
     </div>
   );
 
-function CharacterSelection() {
+export default function CharacterSelection() {
   const [teamId, setTeamId] = useState<string | null>(null);
   const [activePanel, setActivePanel] = useState<{ [key: string]: 'description' | 'scores' }>({
     Megan: 'description',
@@ -959,6 +959,5 @@ return (
       })}
     </div>
 </div>
-);
-};
-export default CharacterSelection;
+  );
+}
