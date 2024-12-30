97d037db3c122638db8460ee4b5fb76c4858cac6
jantrej <jantrejbal7@gmail.com>
2024-12-30 19:50:30 +0100

    Update ScorePanel.tsx

--- src/components/ui/ScorePanel.tsx
+++ src/components/ui/ScorePanel.tsx
@@ -255,13 +255,13 @@ return (
       <div className="flex-grow overflow-y-auto scrollbar-thin">
         <h3 className="text-sm font-semibold mb-2 sticky top-0 bg-white py-2 z-10">
 <div className="mb-1">
-{wasEverCompleted.current ? (
-  "The challenge has been completed. ✅"
-) : (
-  `${Math.max(0, performanceGoals.number_of_calls_average - (metrics?.total_calls || 0))} ${
-    performanceGoals.number_of_calls_average - (metrics?.total_calls || 0) === 1 ? 'call' : 'calls'
-  } left to complete the challenge.`
-)}
+  {wasEverCompleted.current ? (
+    "The challenge has been completed. ✅"
+  ) : (
+    `${Math.max(0, performanceGoals.number_of_calls_average - (metrics?.total_calls || 0))} ${
+      performanceGoals.number_of_calls_average - (metrics?.total_calls || 0) === 1 ? 'call' : 'calls'
+    } left to complete the challenge.`
+  )}
 </div>
           <div>
             Your score from last {metrics?.total_calls || 0} {(metrics?.total_calls || 0) === 1 ? 'call' : 'calls'}:
